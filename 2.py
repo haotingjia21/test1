170ebf8cf89c59316e6acde06f6574a93e861045
haotingjia21 <93146362+haotingjia21@users.noreply.github.com>
2021-10-25 11:40:39 -0400

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+import re
+
+'''
+the modified lines are from 39 - 56 in function all_ofrs()
+'''
+
+for record in SeqIO.parse("spiderman.fasta", 'fasta'):
+    seq = record.seq
+
+rna = seq
+rna_r = rna.reverse_complement()
+rna = str(rna).upper()
+rna_r = str(rna_r).upper()
+
+rna1 = rna[1:]
+rna1_r = str(Seq(rna1).reverse_complement())
+
+rna2 = rna1[1:]
+rna2_r = str(Seq(rna2).reverse_complement())
+
+# split 6 translation frames into codons
+codon1 = [rna[i:i+3] for i in range(0, len(rna), 3)]
+codon2 = [rna_r[i:i+3] for i in range(0, len(rna_r), 3)]
+codon3 = [rna1[i:i+3] for i in range(0, len(rna1), 3)]
+codon4 = [rna1_r[i:i+3] for i in range(0, len(rna1_r), 3)]
+codon5 = [rna2[i:i+3] for i in range(0, len(rna2), 3)]
+codon6 = [rna2_r[i:i+3] for i in range(0, len(rna2_r), 3)]
+
+# extract orfs
+def all_orfs(codon):
+    orfs = list()
+    remaining = codon
+    x = 1
+    while x == 1:
+        start1, start2, start3, start4, start5 = \
+            9999, 9999, 9999, 9999, 9999
+        # ATG, AGT, TTG, GTG, or CTG
+        if 'ATG' in remaining:
+            start1 = remaining.index('ATG')
+        if 'AGT' in remaining:
+            start2 = remaining.index('AGT')
+        if 'TTG' in remaining:
+            start3 = remaining.index('TTG')
+        if 'GTG' in remaining:
+            start4 = remaining.index('GTG')
+        if 'CTG' in remaining:
+            start5 = remaining.index('CTG')
+
+        start = min(start1, start2, start3,
+                    start4, start5)
+
+        if start != 9999:
+            remaining = remaining[start:]
+            end1, end2, end3 = 9999, 9999, 9999
+            if "TAA" in remaining:
+                end1 = remaining.index('TAA')
+            if 'TAG' in remaining:
+                end2 = remaining.index('TAG')
+            if 'TGA' in remaining:
+                end3 = remaining.index('TGA')
+            end = min(end1, end2, end3)
+            orfs.append(remaining[:end+1])
+            remaining = remaining[end:]
+        else:
+            x = 0
+    return orfs
+
+# 6 translation frames' orfs
+orf1 = all_orfs(codon1)
+orf2 = all_orfs(codon2)
+orf3 = all_orfs(codon3)
+orf4 = all_orfs(codon4)
+orf5 = all_orfs(codon5)
+orf6 = all_orfs(codon6)
+
+# filter orfs and translate using table2
+def newOrf(orf):
+    newOrf = []
+    for i in orf:
+        if len(i) > 20:
+            seq = Seq(''.join(i)).translate(table=2)
+            newOrf.append(seq)
+    return newOrf
+
+n1 = newOrf(orf1)
+n2 = newOrf(orf2)
+n3 = newOrf(orf3)
+n4 = newOrf(orf4)
+n5 = newOrf(orf5)
+n6 = newOrf(orf6)
+
+# print all AA's.
+allorfs = n1 + n2 + n3 + n4 + n5
+for i in allorfs:
+    print(str(i))
